cd5dc4716323eca10293440ddf09dbdae0dfe501
shepherd cheung <1220798123@qq.com>
2025-03-06 14:15:12 +0800

    【修改说明 Modification】如果没有收集到数据，认为卡状态未知 【修改人 Modifier】Atlas_zxp 【评审人 Reviewer】luxiang6

--- component/noded/pkg/pingmesh/resulthandler/cmreporter/fault_analyze.go
+++ component/noded/pkg/pingmesh/resulthandler/cmreporter/fault_analyze.go
@@ -160,11 +160,13 @@ func (f *faultReporter) getLastFault() (*api.PubFaultInfo, error) {
 }
 
 func checkFaultCard(infos map[uint]*common.HccspingMeshInfo) state {
-	if len(infos) == 0 {
-		return stateUnknown
-	}
 	hasSuc := false
+	hasDest := false
 	for _, info := range infos {
+		if info.DestNum == 0 {
+			continue
+		}
+		hasDest = true
 		for i := 0; i < info.DestNum; i++ {
 			if info.ReplyStatNum[i] == 0 {
 				return stateUnknown
@@ -173,6 +175,9 @@ func checkFaultCard(infos map[uint]*common.HccspingMeshInfo) state {
 				hasSuc = true
 			}
 		}
+	}
+	if !hasDest {
+		return stateUnknown
 	}
 	if hasSuc {
 		return stateHealthy
