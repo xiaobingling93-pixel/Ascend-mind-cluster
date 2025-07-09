6bd4f3640f77b393f5d3bfc4f4b3abf41c7ae344
lirui238 <2396601465@qq.com>
2025-07-09 08:08:48 +0000

    !1095 【修改说明】[taskd] dt * 【修改说明】[taskd] dt

--- component/taskd/tests/ut/python/framework/agent/ms_mgr/test_msrun_plugin.py
+++ component/taskd/tests/ut/python/framework/agent/ms_mgr/test_msrun_plugin.py
@@ -24,7 +24,7 @@ from taskd.python.toolkit.constants.constants import START_ALL_WORKER_CALLBACK_N
 from taskd.python.toolkit.fault_checker.fault_check import ResetCmData, FaultStatus
 
 
-def getDemoMsPlugin()->MSRunPlugin:
+def getDemoMsPlugin() -> MSRunPlugin:
     ms = MSRunPlugin()
     ms.start_proxy = MagicMock()
     mock_kill = MagicMock()
@@ -66,7 +66,7 @@ class TestMSRunPlugin(unittest.TestCase):
         mock_func = MagicMock(side_effect=lambda x: None)
         ms.register_callbacks(START_WORKER_LIST_CALLBACK_NAME, mock_func)
         ms.wait_to_start = lambda: True
-        ms.start_mindspore_worker_list([8,9,10,11])
+        ms.start_mindspore_worker_list([8, 9, 10, 11])
         mock_func.assert_called_once()
 
     @patch('time.sleep')
@@ -84,7 +84,7 @@ class TestMSRunPlugin(unittest.TestCase):
 
     def test_all_fault_has_recovered_failure_on_not_all_recovered(self):
         ms = MSRunPlugin()
-        ms.fault_ranks = [{'Status': 'recovered'},{'Status': 'unrecovered'},{'hello':'world'}]
+        ms.fault_ranks = [{'Status': 'recovered'}, {'Status': 'unrecovered'}, {'hello': 'world'}]
         res = ms.all_fault_has_recovered()
         self.assertFalse(res)
 
@@ -92,7 +92,7 @@ class TestMSRunPlugin(unittest.TestCase):
     def test_all_fault_has_recovered_failure_on_wrong_version(self, mock_exists):
         mock_exists.return_value = True
         ms = MSRunPlugin()
-        ms.fault_ranks = [{'Status': 'recovered'},{'Status': 'recovered'}]
+        ms.fault_ranks = [{'Status': 'recovered'}, {'Status': 'recovered'}]
         ms.restart_type = constants.VALUE_RESTART_RESCHEDULE_TYPE
         ms.rank_table_version = 1
         ms.read_rank_table_version = lambda: 0
@@ -117,7 +117,7 @@ class TestMSRunPlugin(unittest.TestCase):
                 {'Status': 'recovered', 'RankId': 0},
                 {'Status': 'unrecovered', 'RankId': 1},
                 {'Status': 'fault', 'RankId': 2},
-                {'hello':'world'}
+                {'hello': 'world'}
             ],
             retry_time=2,
             grace_exit=True,
@@ -125,7 +125,7 @@ class TestMSRunPlugin(unittest.TestCase):
             fault_flush=False,
             restart_fault_process=False)
         ms = MSRunPlugin()
-        ms.node_global_rank_ids = [0,1,2,3]
+        ms.node_global_rank_ids = [0, 1, 2, 3]
         ms.pre_retry_time = 1
         res = ms.get_fault_status()
         self.assertTrue(res.is_fault)
@@ -198,7 +198,7 @@ class TestMSRunPlugin(unittest.TestCase):
         ms.update_rank_status(rank_status_dict)
         self.assertEqual(ms.node_global_rank_ids, [16, 17])
         self.assertEqual(ms.all_rank_succeed, False)
-        self.assertEqual(ms.local_rank_to_pid, {16:101,17:110})
+        self.assertEqual(ms.local_rank_to_pid, {16: 101, 17: 110})
         self.assertEqual(ms.rank_status, ms.RANK_STATUS_UNHEALTHY)
 
     def test_update_rank_status_succeed(self):
@@ -210,7 +210,7 @@ class TestMSRunPlugin(unittest.TestCase):
         ms.update_rank_status(rank_status_dict)
         self.assertEqual(ms.node_global_rank_ids, [16, 17])
         self.assertEqual(ms.all_rank_succeed, True)
-        self.assertEqual(ms.local_rank_to_pid, {16:101,17:110})
+        self.assertEqual(ms.local_rank_to_pid, {16: 101, 17: 110})
         self.assertEqual(ms.rank_status, ms.RANK_STATUS_HEALTHY)
 
     def test_update_rank_status_healthy(self):
@@ -222,7 +222,7 @@ class TestMSRunPlugin(unittest.TestCase):
         ms.update_rank_status(rank_status_dict)
         self.assertEqual(ms.node_global_rank_ids, [16, 17])
         self.assertEqual(ms.all_rank_succeed, False)
-        self.assertEqual(ms.local_rank_to_pid, {16:101,17:110})
+        self.assertEqual(ms.local_rank_to_pid, {16: 101, 17: 110})
         self.assertEqual(ms.rank_status, ms.RANK_STATUS_HEALTHY)
 
     @patch('taskd.python.toolkit.fault_checker.fault_check.fault_processor.get_reset_info_from_cm')
@@ -234,9 +234,9 @@ class TestMSRunPlugin(unittest.TestCase):
         mock_cm.return_value = data
         ms = MSRunPlugin()
         ms.update_reset_info()
-        self.assertTrue(ms.fault_ranks, data.fault_ranks)
-        self.assertTrue(ms.retry_time, data.retry_time)
-        self.assertTrue(ms.grace_exit, data.grace_exit)
+        self.assertEqual(ms.fault_ranks, data.fault_ranks)
+        self.assertEqual(ms.retry_time, data.retry_time)
+        self.assertEqual(ms.grace_exit, data.grace_exit)
         self.assertEqual(ms.restart_type, data.restart_type)
         self.assertEqual(ms.restart_fault_process, data.restart_fault_process)
 
@@ -244,13 +244,67 @@ class TestMSRunPlugin(unittest.TestCase):
         ms = MSRunPlugin()
         ms.retry_time = 1
         ms.update_pre_fault_infos()
-        self.assertTrue(ms.pre_retry_time, ms.retry_time)
+        self.assertEqual(ms.pre_retry_time, ms.retry_time)
+
+    @patch('taskd.python.toolkit.fault_checker.fault_check.fault_processor.get_reset_info_from_cm')
+    @patch("taskd.python.framework.agent.ms_mgr.msrun_plugin.calculate_global_rank")
+    def test_wait_to_start_fault_flush(self, mock_calc: MagicMock, mock_cm: MagicMock):
+        data = ResetCmData(fault_ranks=[{'Status': 'recovered', 'RankId': 0}, {'Status': 'unrecovered', 'RankId': 1},
+                                        {'Status': 'fault', 'RankId': 2}, {'hello': 'world'}], retry_time=2,
+                           grace_exit=True, restart_type=constants.VALUE_RESTART_HOTRESET_TYPE, fault_flush=True,
+                           restart_fault_process=False)
+        mock_cm.return_value = data
+        mock_calc.return_value = [8, 9, 10, 11, 12, 13, 14, 15]
+        ms = getDemoMsPlugin()
+        res = ms.wait_to_start()
+        self.assertFalse(res)
+
+    @patch('taskd.python.toolkit.fault_checker.fault_check.fault_processor.get_reset_info_from_cm')
+    @patch("taskd.python.framework.agent.ms_mgr.msrun_plugin.calculate_global_rank")
+    def test_wait_to_start_fault_ranks(self, mock_calc: MagicMock, mock_cm: MagicMock):
+        data = ResetCmData(fault_ranks=[], retry_time=2,
+                           grace_exit=True, restart_type=constants.VALUE_RESTART_HOTRESET_TYPE, fault_flush=False,
+                           restart_fault_process=False)
+        mock_cm.return_value = data
+        mock_calc.return_value = [8, 9, 10, 11, 12, 13, 14, 15]
+        ms = getDemoMsPlugin()
+        res = ms.wait_to_start()
+        self.assertTrue(res)
+
+    @patch('taskd.python.toolkit.fault_checker.fault_check.fault_processor.get_reset_info_from_cm')
+    @patch("taskd.python.framework.agent.ms_mgr.msrun_plugin.calculate_global_rank")
+    def test_wait_to_start_fault(self, mock_calc: MagicMock, mock_cm: MagicMock):
+        data = ResetCmData(fault_ranks=[{'Status': 'recovered', 'RankId': 8},
+                                        {'Status': 'fault', 'RankId': 9}, {'hello': 'world'}], retry_time=2,
+                           grace_exit=True, restart_type=constants.VALUE_RESTART_HOTRESET_TYPE, fault_flush=True,
+                           restart_fault_process=False)
+        mock_cm.return_value = data
+        mock_calc.return_value = [8, 9, 10, 11, 12, 13, 14, 15]
+        ms = getDemoMsPlugin()
+        res = ms.wait_to_start()
+        self.assertFalse(res)
+
+    @patch('taskd.python.toolkit.fault_checker.fault_check.fault_processor.get_reset_info_from_cm')
+    @patch("taskd.python.framework.agent.ms_mgr.msrun_plugin.calculate_global_rank")
+    def test_wait_to_start_success(self, mock_calc: MagicMock, mock_cm: MagicMock):
+        data = ResetCmData(fault_ranks=[{'Status': 'recovered', 'RankId': 8},
+                                        {'Status': 'recovered', 'RankId': 9}, {'hello': 'world'}], retry_time=2,
+                           grace_exit=True, restart_type=constants.VALUE_RESTART_HOTRESET_TYPE, fault_flush=False,
+                           restart_fault_process=False)
+        mock_cm.return_value = data
+        mock_calc.return_value = [8, 9, 10, 11, 12, 13, 14, 15]
+        ms = getDemoMsPlugin()
+        res = ms.wait_to_start()
+        self.assertTrue(res)
+
 
 def mock_exit_raise():
     raise Exception("exit")
 
+
 def mock_fault_status():
     return FaultStatus([], False, False, False, False)
+
 
 if __name__ == '__main__':
     unittest.main()
