8d1fe87eade5974784b5606388d388ab4ce68ebe
shepherd-cheung <1220798123@qq.com>
2025-08-26 20:44:57 +0800

    fix(npu-exporter): 为容器添加只读根文件系统安全配置，将文件创建操作移动到Dockerfile中
    Co-authored-by: shepherd-cheung<1220798123@qq.com>

--- component/npu-exporter/build/run_for_310P_1usoc.sh
+++ component/npu-exporter/build/run_for_310P_1usoc.sh
@@ -17,25 +17,6 @@
 
 set -e
 
-# create soft link for ubuntu image
-os="$(cat /etc/*release* | grep -i "ubuntu")"
-if [[ "$os" != "" ]]
-then
-    echo -e "[INFO]\t $(date +"%F %T:%N")\t use ubuntu image, so create soft link \"/lib64\" for \"/lib\""
-    ln -s /lib /lib64 2>&1 >> /dev/null
-fi
-
-umask 027
-
-echo -e "[INFO]\t $(date +"%F %T:%N")\t create driver's related directory"
-mkdir -m 750 /var/driver -m 750 /var/dmp -m 750 /usr/slog -p -m 750 /home/drv/hdc_ppc
-
-echo -e "[INFO]\t $(date +"%F %T:%N")\t modify owner and permission"
-chown HwDmUser:HwDmUser /var/dmp
-chown HwHiAiUser:HwHiAiUser /var/driver
-chown HwHiAiUser:HwHiAiUser /home/drv/hdc_ppc
-chown HwHiAiUser:HwHiAiUser /usr/slog
-
 # log process run in background
 echo -e "[INFO]\t $(date +"%F %T:%N")\t start slogd server in background"
 su - HwHiAiUser -c "export LD_LIBRARY_PATH=/usr/local/Ascend/driver/lib64/:/usr/lib64 && /var/slogd -d &"
