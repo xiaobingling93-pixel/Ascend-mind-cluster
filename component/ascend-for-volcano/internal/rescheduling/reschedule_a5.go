c018e29719c006cef50e8f8dcb6da0e1a0a797d9
Henry_jihaoen <jihaoen@huawei.com>
2026-01-28 11:34:11 +0800

    【bugfix】【volcano】进程级标签判断修改
    Co-authored-by: Henry_jihaoen<jihaoen@huawei.com>

--- component/ascend-for-volcano/internal/rescheduling/reschedule_a5.go
+++ component/ascend-for-volcano/internal/rescheduling/reschedule_a5.go
@@ -49,7 +49,8 @@ func (reScheduler *ReScheduler) updateFaultJobWhenGraceDeleteSuccess(jobInfo *ap
 }
 
 func (reScheduler *ReScheduler) singlePodReschedulingUpgradeFor910A5(jobInfo *api.JobInfo, fJob *FaultJob) {
-	if jobInfo.PodGroup.Labels[util.SinglePodTag] != util.EnableFunc {
+	if jobInfo.PodGroup.Labels[util.SinglePodTag] != util.EnableFunc &&
+		jobInfo.PodGroup.Labels[util.ProcessRecoverEnable] != util.EnableFunc {
 		return
 	}
 
