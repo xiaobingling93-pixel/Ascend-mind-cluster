24eeca44e96ed6a97b3a03a82a764ca244670527
lirui2381 <2396601465@qq.com>
2026-03-31 20:52:02 +0800

    【BUILD】添加container-manager infer-operator
    Co-authored-by: lirui238<2396601465@qq.com>

--- build/build_all.sh
+++ build/build_all.sh
@@ -58,7 +58,19 @@ fi
 
 cd "$TOP_DIR"/component
 CUR_DIR=$(dirname $(readlink -f $0))
-mind_cluster=("ascend-device-plugin" "ascend-docker-runtime" "ascend-for-volcano" "ascend-operator" "clusterd" "noded" "npu-exporter" "taskd" "mindio")
+mind_cluster=(
+    "ascend-device-plugin"
+    "ascend-docker-runtime"
+    "ascend-for-volcano"
+    "ascend-operator"
+    "clusterd"
+    "container-manager"
+    "infer-operator"
+    "mindio"
+    "noded"
+    "npu-exporter"
+    "taskd"
+)
 cd "$TOP_DIR"/build
 cp -rf "$TOP_DIR"/build/service_config.ini $GOPATH/service_config.ini
 dos2unix *.sh && chmod +x *
